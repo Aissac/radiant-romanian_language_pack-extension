58f0c62766527dc0b1b862f3f4b51e7e2d357381
Cristi Duma <cristi.duma@gmail.com>
2010-05-25 20:10:34 +0300

    it's alive

---
+++ romanian_language_pack_extension.rb
@@ -0,0 +1,8 @@
+class RomanianLanguagePackExtension < Radiant::Extension
+  version "1.0"
+  description "Provides a Romanian translation for the Radiant admin interface"
+  url "http://yourwebsite.com/ro"
+
+  def activate
+  end
+end
